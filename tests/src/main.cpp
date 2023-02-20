d4cd7cb43161eaaf39076d90a859e63ec3071bc4
q-ptr <q-ptr@outlook.com>
2023-02-20 17:10:03 +0200

    Added compiler defines

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -1,6 +1,7 @@
 
 #include <qptr/predef/version.hpp>
 #include <qptr/predef/architecture.hpp>
+#include <qptr/predef/compiler.hpp>
 #include <fmt/core.h>
 
 
@@ -8,6 +9,7 @@ int main()
 {
 	fmt::print("qptr::predef::VERSION         : {}\n", qptr::predef::VERSION.to_string());
 	fmt::print("QPTR_PREDEF_ARCHITECTURE_NAME : {}\n", QPTR_PREDEF_ARCHITECTURE_NAME);
+	fmt::print("QPTR_PREDEF_COMPILER_NAME     : {}\n", QPTR_PREDEF_COMPILER_NAME);
 
 	return 0;
 }
