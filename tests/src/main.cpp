11e9a991ab9b763558e8e8758c46dbb0c862d188
q-ptr <q-ptr@outlook.com>
2023-02-20 17:07:43 +0200

    Added architecture

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -1,11 +1,13 @@
 
 #include <qptr/predef/version.hpp>
+#include <qptr/predef/architecture.hpp>
 #include <fmt/core.h>
 
 
 int main()
 {
-	fmt::print("qptr::predef::VERSION     : {}\n", qptr::predef::VERSION.to_string());
+	fmt::print("qptr::predef::VERSION         : {}\n", qptr::predef::VERSION.to_string());
+	fmt::print("QPTR_PREDEF_ARCHITECTURE_NAME : {}\n", QPTR_PREDEF_ARCHITECTURE_NAME);
 
 	return 0;
 }
